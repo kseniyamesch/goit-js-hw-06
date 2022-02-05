927007ffc14b64055720750d9eec7eca274b5ef0
Kseniia Malyshenko <kseniyamesch0101@gmail.com>
2022-02-05 14:43:14 +0200

    Добавляет решение Task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listRef = document.querySelector('#ingredients');
+const createElements = element => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = element;
+  return itemRef;
+};
+const elementListRef = ingredients.map(ingredient =>
+  createElements(ingredient),
+);
+
+listRef.append(...elementListRef);
