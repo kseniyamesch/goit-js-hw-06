2a7167a24facea3bf89177fbead61804451276d7
Kseniia Malyshenko <kseniyamesch0101@gmail.com>
2022-02-06 17:26:11 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyRef = document.querySelector('body');
+console.log(bodyRef)
+const btnRef = document.querySelector('.change-color');
+console.log(btnRef);
+  const spanRef = document.querySelector('.color');
+
+  btnRef.addEventListener('click', (onChangeColor));
+
+  function onChangeColor (event) {
+    const color = bodyRef.style.backgroundColor = getRandomHexColor();
+    spanRef.textContent = `${color}`
+  }
