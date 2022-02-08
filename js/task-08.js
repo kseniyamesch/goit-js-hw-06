abd0e6e1ab9bec8a62cb9a76a89c058f2aa3fbf9
Kseniia Malyshenko <kseniyamesch0101@gmail.com>
2022-02-08 18:49:09 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -6,14 +6,18 @@ form.addEventListener('submit', (onFormSubmit));
 function onFormSubmit (event) {
     event.preventDefault();
     // alert('hbkj')
-    const emailVal = event.currentTarget.elements.email.value;
-const passwordVal = event.currentTarget.elements.password.value;
+    const email = event.currentTarget.elements.email.value;
+const password = event.currentTarget.elements.password.value;
     // console.dir(emailVal)
-    if (emailVal === "" || passwordVal === "") {
+    if (email === "" || password === "") {
         alert('Заполните пустые поля');
     }
 else {
-    const formData = new FormData(event.currentTarget);
+    // const formData = new FormData(event.currentTarget);
+    const formData = {
+email,
+password
+    }
     console.log(formData);
 form.reset();
 }}
